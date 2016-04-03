a203ac6189beddf4f0a734973a77ef6e92fd3b43
miguel <miganrobles@gmail.com>
2016-04-03 12:52:45 +0200

    creada clase alumno con sus atributos

---
+++ Alumno.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class Alumno here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Alumno
+{
+    // nombre del alumno
+    private String nombre;
+    // edad del alumno
+    private int edad;
+    // numero de clase
+    private int numero;
+    // asigna numero de alumno
+    private static int numeroAlumno = 1;
+    // nota minima para aprobar
+    public static final int NOTA_MINIMA_APROBADO = 5;
+    
+    /**
+     * Constructor for objects of class Alumno
+     */
+    public Alumno()
+    {
+        
+    }
+}

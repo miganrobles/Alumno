bd6b40b94b63c17650456d4d7132c5e4eeff7958
miguel <miganrobles@gmail.com>
2016-04-03 13:20:56 +0200

    Añadido atributo para almacenar las notas y codificado el constructor de la clase Alumno

--- Alumno.java
+++ Alumno.java
@@ -12,17 +12,23 @@ public class Alumno
     // edad del alumno
     private int edad;
     // numero de clase
-    private int numero;
+    private int numeroId;
     // asigna numero de alumno
     private static int numeroAlumno = 1;
     // nota minima para aprobar
     public static final int NOTA_MINIMA_APROBADO = 5;
+    // notas del alumno
+    private ArrayListInt notas;
     
     /**
      * Constructor for objects of class Alumno
      */
-    public Alumno()
+    public Alumno(String nombre, int edad)
     {
-        
+        this.nombre = nombre;
+        this.edad = edad;
+        numeroId = numeroAlumno;
+        numeroAlumno++;
+        notas = new ArrayListInt();
     }
 }
